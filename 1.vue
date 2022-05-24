91ae84172f8d2935232505ea3c7ee82490e687e1
wk <1607699239@qq.com>
2022-05-24 09:55:57 +0800

    test

---
+++ 1.vue
@@ -0,0 +1,16 @@
+<template></template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      x: 'q',
+      a: 10,
+      c: 1,
+    }
+  },
+}
+</script>
+
+<style></style>
---
+++ test.js
@@ -0,0 +1,7 @@
+import vue from 'vue'
+
+function star() {
+  console.log(11)
+}
+
+console.log(1)
